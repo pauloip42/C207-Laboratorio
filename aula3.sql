82efa87eb57ead0dfada56220a8c8571a049e5b9
Paulo Lopes <phlj.junior@gmail.com>
2022-02-22 09:11:19 -0300

    Select

--- aula3.sql
+++ aula3.sql
@@ -39,8 +39,23 @@ create table if not exists usuario(
   #SELECT colunas FROM tabelas WHERE lógica;
   SELECT * FROM usuario; #busca todos os dados
   SELECT * FROM Cliente;
-
-/*
+  
+  #BUSCA COM FILTROS
+  SELECT nome, idade, cidade FROM Cliente WHERE cidade = 'Itajubá';
+  #SELECT COM OPERADORES LÓGICOS
+  SELECT nome, cidade, totalCompra FROM Cliente WHERE totalCompra >=100 AND NOT cidade = 'Itajubá';
+  #SELECT com funções prontas
+  SELECT avg(totalCompra) FROM Cliente;
+  SELECT max(totalCompra) FROM Cliente;
+  
+  #SELECT com filtro para campos de texto
+  SELECT * FROM Cliente WHERE nome LIKE 'Jo____%';
+  SELECT * FROM Cliente WHERE nome LIKE '%a%';
+  
+  
+  
+  
+  #
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('12379843256','João Lucas',24,'9845-6952','Pouso Alegre',145);
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('95487361584','Joana',45,'9972-3348','Itajubá',130.25);
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('65482751499','Pedro',33,'9822-5316','Itajubá',78.9);
@@ -48,5 +63,5 @@ create table if not exists usuario(
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('26326578459','Mariana',28,'8437-6659','Barbacena',42.5);
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('76125894362','Jonathan',38,'9233-7453','Pouso Alegre',85.4);
   INSERT INTO Cliente(cpf,nome,idade,telefone,cidade,TotalCompra) VALUES ('46582739321','Ana Maria',57,'9118-4742','Varginha',92);
-  */
+
   
